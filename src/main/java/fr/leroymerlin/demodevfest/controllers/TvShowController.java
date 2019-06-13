64bcea8f4af04d373359b48e5200ad5fa85f77bf
jdesousa <julien.desousa@leroymerlin.fr>
2019-06-13 09:40:02 +0200

    feat(controller): add loggger for each 1000 elements in findAll

--- src/main/java/fr/leroymerlin/demodevfest/controllers/TvShowController.java
+++ src/main/java/fr/leroymerlin/demodevfest/controllers/TvShowController.java
@@ -4,7 +4,6 @@ import fr.leroymerlin.demodevfest.model.TvShow;
 import fr.leroymerlin.demodevfest.service.TvShowService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.time.StopWatch;
 import org.springframework.http.MediaType;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +15,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 @AllArgsConstructor
@@ -36,23 +36,32 @@ public class TvShowController {
 
 	@GetMapping(value = "/tvShows", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
 	public Flux<TvShow> getTvShows(@RequestParam(value = "ids", required = false) List<String> ids) {
-		StopWatch s = new StopWatch();
+		AtomicInteger count = new AtomicInteger(0);
 
 		if (!CollectionUtils.isEmpty(ids)) {
 			return tvShowService.findByIds(ids);
 		} else {
 			return tvShowService.findAll()
-								.doOnNext(tvShow -> log.info("get {}", tvShow))
-								.doOnError(throwable -> log.error("Error"))
-								.doOnTerminate(() -> log.info("Terminate maybe with errors"))
-								.doOnComplete(() -> log.info("Terminate without errors"))
-								.doOnCancel(() -> log.info("Cancelled."))
-								.doOnSubscribe(subscription -> s.start());
+								.doOnNext(tvShow -> {
+									if (count.incrementAndGet() % 1000 == 0) {
+										log.info("Count {}", count.get());
+									}
+								})
+								.doFinally(s -> log.info("Count {}", count.get()));
+
 		}
 	}
 
 	@GetMapping(value = "/tvShows/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public Mono<TvShow> getTvShowById(@PathVariable String id) {
-		return tvShowService.findById(id);
+		return tvShowService.findById(id)
+							.doOnNext(tvShow -> log.info("get {}", tvShow))
+							.doOnError(throwable -> log.error("Error"))
+							.doOnTerminate(() -> log.info("Terminate maybe with errors"))
+							.doOnSuccess(tvShow -> log.info("Terminate without errors"))
+							.doOnCancel(() -> log.info("Cancelled."))
+							.doOnSubscribe(subscription -> log.info("Subscription"));
+
 	}
+
 }
